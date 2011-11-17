c75fa6aa700c60ed2b5fc146d3d869c7ec95ccb8
Thomas Crepain <info@thomascrepain.be>
2011-11-17 14:57:39 +0100

    default settingspage added

---
+++ settings.default.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Database configuration
+ */
+// Type of connection
+define('DATABASE_TYPE', 'mysql');
+// Database name
+define('DATABASE_DATABASE', '<database name>');
+// Database host
+define('DATABASE_HOSTNAME', '<hostname>');
+// Database port
+define('DATABASE_PORT', '3306');
+// Database username
+define('DATABASE_USERNAME', '<username>');
+// Datebase password
+define('DATABASE_PASSWORD', '<password>');
+
+/**
+ * Template properties
+ */
+// Template directory
+define('TEMPLATE_DIRECTORY', 'templates');
+
+/**
+ * Author configuration
+ */
+// Author name
+define('AUTHOR_NAME', '<author>');
+// Author email
+define('AUTHOR_EMAIL', '<email>')
+?>
